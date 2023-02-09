cbf7d9c5973bd9f2c9505415edd740809ec1d0d2
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-09 15:59:00 -0800

    Navbar and Google Maps API

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,9 +1,48 @@
+import React, { Component } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
 function Map() {
-    return (
-      <div>
-        <h1>This is the Map.</h1>
-      </div>
-    )
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyACJwjUE7Z5Xju4G7LYuU865-M22090SjE"
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+      // This is just an example of getting and using the map instance!!! don't just blindly copy!
+      const bounds = new window.google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+
+      setMap(map)
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+      setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>
+    ) : <></>
   }
 
   export default Map
