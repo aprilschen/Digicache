05565939a93cbc19274bd167725b6ba487aed1a4
Shiwei Chen <april.chen.0615@gmail.com>
2023-02-08 21:59:21 -0800

    Material UI Configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,12 @@ import Header from './components/Header'
 import Map from './components/Map'
 import Form from './components/Form'
 import Footer from './components/Footer'
-import './App.css'
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Header/>
       <Map/>
       <Form/>
