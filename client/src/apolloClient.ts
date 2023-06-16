c6cdaa0b6c610bf8b8cb3700f669692fe2f053d2
Shiwei Chen <april.chen.0615@gmail.com>
2023-06-16 16:41:47 -0700

    Implemented deployed ELB Backend calls

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:80/',
+  uri: 'http://digicache-docker-server.us-west-1.elasticbeanstalk.com/',
   cache: new InMemoryCache(),
 });
 
