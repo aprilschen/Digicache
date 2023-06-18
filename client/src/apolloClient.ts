e6268ca8be40717bda58247caa5d2560051f6e9d
Shiwei Chen <april.chen.0615@gmail.com>
2023-06-18 16:41:55 -0700

    server architecture

--- client/src/apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://digicache-docker-server.us-west-1.elasticbeanstalk.com/',
-  cache: new InMemoryCache(),
-});
-
-export default client;
